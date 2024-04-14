6ab5d43202a834128e960bf8dda08f7865875d29
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-04-15 01:31:57 +0530

    done

---
+++ src/apollo/mutations/ens.js
@@ -0,0 +1,58 @@
+import { setupENS } from '@ensdomains/ui'
+import { isENSReadyReactive } from '../reactiveVars'
+
+let ens = {},
+  registrar = {},
+  ensRegistryAddress = undefined
+
+export async function setup({
+  reloadOnAccountsChange,
+  enforceReadOnly,
+  enforceReload,
+  customProvider,
+  customNetwork = {
+    chainId: 2370,
+    name: 'Nexis',
+    ensAddress: '0xA6aaC368a4f22FAbF1076946edC8fBfBD5C63c87'
+  },
+
+  ensAddress = '0xA6aaC368a4f22FAbF1076946edC8fBfBD5C63c87'
+}) {
+  let option = {
+    reloadOnAccountsChange: false,
+    enforceReadonly: enforceReadOnly,
+    enforceReload: enforceReload,
+    customProvider: customProvider,
+    customNetwork: customNetwork,
+    ensAddress: ensAddress
+  }
+  try {
+    const {
+      ens: ensInstance,
+      registrar: registrarInstance,
+      providerObject
+    } = await setupENS(option)
+
+    console.log(option)
+
+    ens = ensInstance
+    registrar = registrarInstance
+    ensRegistryAddress = ensAddress
+    isENSReadyReactive(true)
+    return { ens, registrar, providerObject }
+  } catch (error) {
+    console.log('encountered error===', error)
+  }
+}
+
+export function getRegistrar() {
+  return registrar
+}
+
+export function getEnsAddress() {
+  return ensRegistryAddress
+}
+
+export default function getENS() {
+  return ens
+}
---
+++ src/rpcUrl.js
@@ -0,0 +1,1 @@
+export const rpcUrl = 'https://evm-testnet.nexis.network'
